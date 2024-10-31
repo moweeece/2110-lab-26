788a84ee87d4fdf331dd06d20746df1bec50e307
moweeece <mjespinosa990@gmail.com>
2024-10-30 21:24:17 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <iomanip>   // for setw()
 #include <string>
 #include <algorithm>
+
 using namespace std;
 using namespace std::chrono;
 
@@ -30,104 +31,116 @@ void deleteDataVec(vector<string>&);
 void deleteDataList(list<string>&);
 void deleteDataSet(set<string>&);
 
+const int NUM_SIMULATIONS = 15;
 
 int main() {
-    // vector, list, and set declarations
-    vector<string> vec1;
-    list<string> list1;
-    set<string> set1;
+    // 3D array declaration with 4 operations and 3 containers
+    long arrayResults[4][3][NUM_SIMULATIONS] = {0}; 
 
     const string fileName = "codes.txt";
 
-    // header output
+    // for loop to run simulation a certain number of times
+    for (int i = 0; i < NUM_SIMULATIONS; i++)
+    {
+        // vector, list, and set declarations
+        vector<string> vec1;
+        list<string> list1;
+        set<string> set1;
+
+
+        // READING DATA
+        // Reading Data from Vector
+        auto startReadVec = high_resolution_clock::now();
+        readDataVec(fileName, vec1);               // call readData function and pass the name of the file and each container type
+        auto endReadVec = high_resolution_clock::now();
+        arrayResults[0][0][i] = duration_cast<milliseconds>(endReadVec - startReadVec).count();
+
+        // Reading Data from List
+        auto startReadList = high_resolution_clock::now();
+        readDataList(fileName, list1);               // call readData function and pass the name of the file and each container type
+        auto endReadList = high_resolution_clock::now();
+        arrayResults[0][1][i] = duration_cast<milliseconds>(endReadList - startReadList).count();
+
+        // Reading Data from Vector
+        auto startReadSet = high_resolution_clock::now();
+        readDataSet(fileName, set1);               // call readData function and pass the name of the file and each container type
+        auto endReadSet = high_resolution_clock::now();
+        arrayResults[0][2][i] = duration_cast<milliseconds>(endReadSet - startReadSet).count();
+
+
+
+        // SORTING DATA
+        // Sort Vector Data
+        auto startSortVec = high_resolution_clock::now();
+        sortDataVec(vec1);                               // call sortData function and pass vector (no set since set is already sorted)
+        auto endSortVec = high_resolution_clock::now();
+        arrayResults[1][0][i] = duration_cast<milliseconds>(endSortVec - startSortVec).count();
+
+        // Sort List Data
+        auto startSortList = high_resolution_clock::now();
+        sortDataList(list1);                               // call sortData function and pass vector (no set since set is already sorted)
+        auto endSortList = high_resolution_clock::now();
+        arrayResults[1][1][i] = duration_cast<milliseconds>(endSortList - startSortList).count();
+
+        // Sort Set Data
+        arrayResults[1][2][i] = 0;  // because an element is automatically sorted in a set
+        
+
+
+        // INSERTING DATA
+        // Insert Data to Vector
+        auto startInsertVec = high_resolution_clock::now();
+        insertDataVec(vec1);                      // call insertData function to insert data to vector
+        auto endInsertVec = high_resolution_clock::now();
+        arrayResults[2][0][i] = duration_cast<milliseconds>(endInsertVec - startInsertVec).count();
+
+        // Insert Data to List
+        auto startInsertList = high_resolution_clock::now();
+        insertDataList(list1);                      // call insertData function to insert data to list
+        auto endInsertList = high_resolution_clock::now();
+        arrayResults[2][1][i] = duration_cast<milliseconds>(endInsertList - startInsertList).count();
+
+        // Insert Data to Set
+        auto startInsertSet = high_resolution_clock::now();
+        insertDataSet(set1);                      // call insertData function to insert data to set
+        auto endInsertSet = high_resolution_clock::now();
+        arrayResults[2][2][i] = duration_cast<milliseconds>(endInsertSet - startInsertSet).count();
+
+
+
+        // DELETEING DATA
+        // Delete Data from Vector
+        auto startDeleteVec = high_resolution_clock::now();
+        deleteDataVec(vec1);                     // call the deleteData function to delete data from vector
+        auto endDeleteVec = high_resolution_clock::now();
+        arrayResults[3][0][i] = duration_cast<milliseconds>(endDeleteVec - startDeleteVec).count();
+
+        // Delete Data from List
+        auto startDeleteList = high_resolution_clock::now();
+        deleteDataList(list1);                     // call the deleteData function to delete data from list
+        auto endDeleteList = high_resolution_clock::now();
+        arrayResults[3][1][i] = duration_cast<milliseconds>(endDeleteList - startDeleteList).count();
+
+        // Delete Data from Set
+        auto startDeleteSet = high_resolution_clock::now();
+        deleteDataSet(set1);                     // call the deleteData function to delete data from set
+        auto endDeleteSet = high_resolution_clock::now();
+        arrayResults[3][2][i] = duration_cast<milliseconds>(endDeleteSet - startDeleteSet).count();
+
+    }
+
+    
+    // Read time output
+    cout << setw(15) << "Number of Simulations: " << NUM_SIMULATIONS << endl;
     cout << setw(15) << "Operation" << setw(15) << "Vector" << setw(15) << "List" << setw(15) << "Set" << endl;
 
-    // READING DATA
-    // Reading Data from Vector
-    auto startReadVec = high_resolution_clock::now();
-    readDataVec(fileName, vec1);               // call readData function and pass the name of the file and each container type
-    auto endReadVec = high_resolution_clock::now();
-    auto durationReadVec = duration_cast<milliseconds>(endReadVec - startReadVec);
+    // average the results
 
-    // Reading Data from List
-    auto startReadList = high_resolution_clock::now();
-    readDataList(fileName, list1);               // call readData function and pass the name of the file and each container type
-    auto endReadList = high_resolution_clock::now();
-    auto durationReadList = duration_cast<milliseconds>(endReadList - startReadList);
 
-    // Reading Data from Vector
-    auto startReadSet = high_resolution_clock::now();
-    readDataSet(fileName, set1);               // call readData function and pass the name of the file and each container type
-    auto endReadSet = high_resolution_clock::now();
-    auto durationReadSet = duration_cast<milliseconds>(endReadSet - startReadSet);
-
-    // Read time output
     cout << setw(15) << "Read" << setw(15) << durationReadVec.count() << setw(15) << durationReadList.count() << setw(15) << durationReadSet.count() << endl;
-
-
-    // SORTING DATA
-    // Sort Vector Data
-    auto startSortVec = high_resolution_clock::now();
-    sortDataVec(vec1);                               // call sortData function and pass vector (no set since set is already sorted)
-    auto endSortVec = high_resolution_clock::now();
-    auto durationSortVec = duration_cast<milliseconds>(endSortVec - startSortVec);
-
-    // Sort List Data
-    auto startSortList = high_resolution_clock::now();
-    sortDataList(list1);                               // call sortData function and pass vector (no set since set is already sorted)
-    auto endSortList = high_resolution_clock::now();
-    auto durationSortList = duration_cast<milliseconds>(endSortList - startSortList);
-
-    // Sort time output
-    int durationSortSet = 0;  // because an element is automatically sorted in a set
     cout << setw(15) << "Sort" << setw(15) << durationSortVec.count() << setw(15) << durationSortList.count() << setw(15) << durationSortSet << endl;
-
-
-    // INSERTING DATA
-    // Insert Data to Vector
-    auto startInsertVec = high_resolution_clock::now();
-    insertDataVec(vec1);                      // call insertData function to insert data to vector
-    auto endInsertVec = high_resolution_clock::now();
-    auto durationInsertVec = duration_cast<milliseconds>(endInsertVec - startInsertVec);
-
-    // Insert Data to List
-    auto startInsertList = high_resolution_clock::now();
-    insertDataList(list1);                      // call insertData function to insert data to list
-    auto endInsertList = high_resolution_clock::now();
-    auto durationInsertList = duration_cast<milliseconds>(endInsertList - startInsertList);
-
-    // Insert Data to Set
-    auto startInsertSet = high_resolution_clock::now();
-    insertDataSet(set1);                      // call insertData function to insert data to set
-    auto endInsertSet = high_resolution_clock::now();
-    auto durationInsertSet = duration_cast<milliseconds>(endInsertSet - startInsertSet);
-
-    // Insert time output
     cout << setw(15) << "Insert" << setw(15) << durationInsertVec.count() << setw(15) << durationInsertList.count() << setw(15) << durationInsertSet.count() << endl;
-
-
-    // DELETEING DATA
-    // Delete Data from Vector
-    auto startDeleteVec = high_resolution_clock::now();
-    deleteDataVec(vec1);                     // call the deleteData function to delete data from vector
-    auto endDeleteVec = high_resolution_clock::now();
-    auto durationDeleteVec = duration_cast<milliseconds>(endDeleteVec - startDeleteVec);
-
-     // Delete Data from List
-    auto startDeleteList = high_resolution_clock::now();
-    deleteDataList(list1);                     // call the deleteData function to delete data from list
-    auto endDeleteList = high_resolution_clock::now();
-    auto durationDeleteList = duration_cast<milliseconds>(endDeleteList - startDeleteList);
-
-     // Delete Data from Set
-    auto startDeleteSet = high_resolution_clock::now();
-    deleteDataSet(set1);                     // call the deleteData function to delete data from set
-    auto endDeleteSet = high_resolution_clock::now();
-    auto durationDeleteSet = duration_cast<milliseconds>(endDeleteSet - startDeleteSet);
-
-    // Delete time output
     cout << setw(15) << "Delete" << setw(15) << durationDeleteVec.count() << setw(15) << durationDeleteList.count() << setw(15) << durationDeleteSet.count() << endl;
-     
 
     return 0;
 }
